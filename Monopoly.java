a4c3490e28e4b210303892d08ebaca3ee7a24374
ennamalone <enna.malone@ucdconnect.ie>
2017-02-18 15:17:18 +0000

    Update Monopoly.java
    Help command added

--- Monopoly.java
+++ Monopoly.java
@@ -1,76 +1,93 @@
 import java.util.ArrayList;
 
-public class Monopoly {
-
+public class Monopoly
+{
 	public static final int MAX_NUM_PLAYERS = 6;
 	public static final int NUM_SQUARES = 40;
 	public static final int rent = 50;
 	public static final int price = 450;
-	
-	
+
 	private ArrayList<Player> players = new ArrayList<Player>();
 	private Money money = new Money();
 	private UI ui = new UI(players);
-	
-	
-	Monopoly () {
-		for (int p=0; p<MAX_NUM_PLAYERS; p++) {
+	private
+
+	Monopoly() 
+	{
+		for (int p=0; p<MAX_NUM_PLAYERS; p++) 
+		{
 			players.add(new Player());
 		}		
 		ui.display();
 		return;
 	}
-	
-	private void tour () {
+
+	private void tour()
+	{
 		ui.displayString("TOUR MODE");
-		for (int p=0; p<MAX_NUM_PLAYERS; p++) {
-			for (int i=0; i<NUM_SQUARES; i++) {
+		for (int p=0; p<MAX_NUM_PLAYERS; p++) 
+		{
+			for (int i=0; i<NUM_SQUARES; i++) 
+			{
 				players.get(p).move(+1);
 				ui.display();
-				try {
+				try 
+				{
 					Thread.sleep(500);
-				} catch (InterruptedException e) {
+				} catch (InterruptedException e) 
+				{
 					System.out.println("Sleep exeception.");
 				} 
 			}
 		}
 		return;
 	}
-	
-	private void echo () {
+
+	private void echo()
+	{
 		String command;
 		ui.display();
 		ui.displayString("ECHO MODE");
-		do {
+		do 
+		{
 			command = ui.getCommand();
 			ui.displayString(command);
-			if(command.equals("balance"))
-				{	
-					 // calls new class money
-					String numberAsString = Integer.toString(money.getBalance());
-					ui.displayString(numberAsString);	// prints out 1500 in response to balance entered
-				}
-			if(command.equals("pay rent"))
+
+			if(command.equalsIgnoreCase("balance")) // works no matter what case is used when typing the word
 			{	
-				 // calls new class money
+				// calls new class money
+				String numberAsString = Integer.toString(money.getBalance());
+				ui.displayString(numberAsString);	// prints out 1500 in response to balance entered
+			}
+
+			if(command.equalsIgnoreCase("pay rent"))
+			{	
+				// calls new class money
 				String numberAsString1 = Integer.toString(money.payRent());
 				ui.displayString(numberAsString1);	// prints out 1500 in response to balance entered
 			}
-			if(command.equals("buy"))
+
+			if(command.equalsIgnoreCase("buy"))
 			{	
-				 // calls new class money
+				// calls new class money
 				String numberAsString2 = Integer.toString(money.buyProperty());
 				ui.displayString(numberAsString2);	// prints out 1500 in response to balance entered
 			}
-			
+
+			if(command.equalsIgnoreCase("help"))
+			{
+				String validCommands = ">Accepted commands are: BALANCE, BUY, PAY RENT, HELP, PROPERTY, ROLL";
+				ui.displayString(validCommands);
+			}
 		} 
 		while (!command.equals("quit"));
 		return;
 	}
-	
-	public static void main (String args[]) {	
+
+	public static void main (String args[])
+	{	
 		Monopoly game = new Monopoly();		
-		//game.tour();
+//		game.tour();
 		game.echo();
 		return;
 	}
