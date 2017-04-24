155c314e55b899ac9c0e1294d5d7d8f5d8355730
Cian Kelly <cian.kelly@ucdconnect.ie>
2017-04-24 18:09:23 +0100

    Add files via upload

---
+++ meanGirls.java
@@ -0,0 +1,28 @@
+
+public class meanGirls implements Bot {
+	
+	// The public API of YourTeamName must not change
+	// You cannot change any other classes
+	// YourTeamName may not alter the state of the board or the player objects
+	// It may only inspect the state of the board and the player objects
+
+	
+	meanGirls (BoardAPI board, PlayerAPI player, DiceAPI dice) {
+		return;
+	}
+	
+	public String getName () {
+		return "meanGirls";
+	}
+	
+	public String getCommand () {
+		// Add your code here
+			return "roll";
+	}
+	
+	public String getDecision () {
+		// Add your code here
+		return "pay";
+	}
+	
+}
